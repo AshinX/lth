e656a326ebda5456a1c20acc123dd2927376b778
fy249085 <fy249085@gongdao.com>
2021-04-08 20:55:18 +0800

    1

--- src/main/java/com/fy/lth/commands/LinuxCommands.java
+++ src/main/java/com/fy/lth/commands/LinuxCommands.java
@@ -1,6 +1,7 @@
 package com.fy.lth.commands;
 
 import com.fy.lth.common.ChinesSizeConstraints;
+import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.table.BorderStyle;
@@ -15,16 +16,30 @@ import org.springframework.shell.table.TableModelBuilder;
  * @date 2021/4/1
  */
 @ShellComponent
+@ShellCommandGroup(value = "linux")
 public class LinuxCommands {
 
-    @ShellMethod("查找关键字")
-    public Table grep() {
+    @ShellMethod(value = "grepTable 导航", key = {"grepTable"})
+    public Table grepTable() {
         TableModelBuilder builder = new TableModelBuilder<String>();
-        builder.addValue("grep -v \"leo\" /etc/passwd").addValue("搜索不包含 leo 字符串的行").addValue(" ");
-
+        builder.addRow().addValue("command  ").addValue("explain  ");
+        builder.addRow().addValue("grep -v \"leo\"  file").addValue("搜索file中不包含 leo 字符串的行");
+        //builder.addRow().addValue(" Summary JVM monitoring").addValue(" ").addValue("https://www.cnblogs.com/wxisme/p/9878494.html");
+        //builder.addRow().addValue(" jps -l").addValue("jps -l -v").addValue("查看java进程");
+        //builder.addRow().addValue("jstat -gcutil pid 1000 3(3 times every 1s)").addValue("percentage used (man jstat)").addValue("https://blog.csdn.net/fenglibing/article/details/6321453 ");
+        //builder.addRow().addValue("jstat -gc pid 1000 3").addValue("actual value ").addValue("https://www.iteye.com/blog/bijian1013-2221351");
+        //builder.addRow().addValue("jstack -l pid").addValue("thread snapshot,deadlock,loop").addValue("http://www.hollischuang.com/archives/110");
+        //builder.addRow().addValue("jstack -l -F  pid").addValue("").addValue("to force a thread dump");
+        //builder.addRow().addValue("jstack -l -m  pid").addValue(" ").addValue("to print both java and native frames (mixed mode)");
+        //builder.addRow().addValue("jmap -dump:live,format=b,file=/tmp/livedump.hprof pid").addValue("").addValue("https://www.jianshu.com/p/a4ad53179df3");
+        //builder.addRow().addValue("jmap -dump:format=b,file=/tmp/dump.hprof pid").addValue("").addValue(" ");
+        //builder.addRow().addValue("jmap -heap pid").addValue("memory usage information of each memory area").addValue("内存占用统计");
+        //builder.addRow().addValue("jmap -histo:live pid").addValue("show statistics for objects in the heap").addValue("活着对象统计");
+        //builder.addRow().addValue("jmap -clstats pid").addValue("to print class loader statistics").addValue("类加载器信息");
+        //builder.addRow().addValue("jmap -finalizerinfo pid").addValue("print information on objects awaiting finalization").addValue("等待终结的对象");
         TableModel tableModel = builder.build();
         TableBuilder tableBuilder = new TableBuilder(tableModel);
         tableBuilder.on(CellMatchers.table()).addSizer(new ChinesSizeConstraints()).and();
-        return tableBuilder.addFullBorder(BorderStyle.fancy_light_triple_dash).build();
+        return tableBuilder.addFullBorder(BorderStyle.fancy_light).build();
     }
 }
---
+++ src/main/java/com/fy/lth/config/MysqlConfig.java
@@ -0,0 +1,49 @@
+package com.fy.lth.config;
+
+import org.springframework.shell.standard.ShellCommandGroup;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.table.BorderStyle;
+import org.springframework.shell.table.CellMatchers;
+import org.springframework.shell.table.Table;
+import org.springframework.shell.table.TableBuilder;
+import org.springframework.shell.table.TableModel;
+import org.springframework.shell.table.TableModelBuilder;
+
+import com.fy.lth.common.ChinesSizeConstraints;
+
+/**
+ * @author fengyue
+ * @date 2021/4/8
+ */
+@ShellComponent
+@ShellCommandGroup(value = "mysqlconfig")
+public class MysqlConfig {
+
+    @ShellMethod(value = "mysqlConfigTable 导航", key = {"mysqlConfigTable"})
+    public Table mysqlConfigTable() {
+        TableModelBuilder builder = new TableModelBuilder<String>();
+        builder.addRow().addValue("config params").addValue("explain");
+        builder.addRow().addValue("innodb_read_io_threads =4").addValue("io读线程数");
+        builder.addRow().addValue("innodb_write_io_threads = 4").addValue("io写线程数");
+        builder.addRow().addValue("innodb_purge_threads = 1").addValue("purge线程用来回收事物提交后用不到的undo.log页");
+        builder.addRow().addValue("innodb_buffer_pool_size").addValue("设置数据库缓冲池大小");
+        builder.addRow().addValue("innodb_buffer_pool_instance").addValue("设置数据库缓冲池实例个数");
+
+        builder.addRow().addValue("innodb_old_blocks_pct").addValue("将新读取到的页放置在整个lru列表的百分比位置（midpoint）。以此点将lru列表分为热点/非热点数据");
+        builder.addRow().addValue("innodb_old_blocks_time").addValue("表示页读取到midpoint位置后需要等待多久才转化为热点数据");
+        builder.addRow().addValue("innodb_log_buffer_size = 8M").addValue("重做日志redolog缓冲大小");
+
+
+
+
+
+
+        TableModel tableModel = builder.build();
+        TableBuilder tableBuilder = new TableBuilder(tableModel);
+        tableBuilder.on(CellMatchers.table()).addSizer(new ChinesSizeConstraints()).and();
+        return tableBuilder.addFullBorder(BorderStyle.fancy_light).build();
+    }
+
+
+}
